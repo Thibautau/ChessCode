109af17eb8b2891cdb1c80a604df796210d120ff
Poirrier Peter <peterdesete@gmail.com>
2024-11-06 13:39:05 +0100

    docs: Ajout de la doc sur Zobrist

--- Zobrist.cpp
+++ Zobrist.cpp
@@ -5,9 +5,25 @@
 #include "Zobrist.h"
 #include <random>
 
+
+/**
+ * Table de hachage Zobrist.
+ * Chaque case du tableau représente un emplacement sur l'échiquier (64 cases)
+ * et chaque pièce a 12 valeurs possibles (une pour chaque type de pièce, y compris les couleurs).
+ */
 std::vector<std::vector<uint64_t>> Zobrist::zobristTable(64, std::vector<uint64_t>(12));
+
+/**
+ * Variable qui stocke la valeur de hachage pour le tour des noirs.
+ */
 uint64_t Zobrist::zobristBlackTurn;
 
+/**
+ * Initialise la table Zobrist avec des valeurs aléatoires.
+ * La fonction génère des nombres aléatoires et les assigne à la table de hachage.
+ * Un générateur de nombres aléatoires est utilisé
+ * pour garantir l'unicité des valeurs de hachage.
+ */
 void Zobrist::initZobrist() {
     std::random_device rd;
     std::mt19937_64 gen(rd());
@@ -21,6 +37,15 @@ void Zobrist::initZobrist() {
     zobristBlackTurn = dis(gen);
 }
 
+/**
+ * Calcule la valeur de hachage Zobrist pour un état donné de l'échiquier.
+ * La fonction prend en compte les pièces sur l'échiquier et le tour en cours (noir ou blanc).
+ * Le résultat est une valeur de 64 bits représentant l'état du jeu.
+ *
+ * @param board Le tableau représentant l'échiquier (64 cases).
+ * @param isBlackTurn Booléen indiquant si c'est le tour des noirs.
+ * @return La valeur de hachage Zobrist de l'état actuel de l'échiquier.
+ */
 uint64_t Zobrist::computeZobristHash(const std::vector<int>& board, bool isBlackTurn) {
     uint64_t h = 0;
     if (isBlackTurn) {
--- Zobrist.h
+++ Zobrist.h
@@ -11,19 +11,6 @@
 
 class Zobrist {
 private:
-    const int PION_BLANC  = 1;
-    const int TOUR_BLANC = 2;
-    const int CAVALIER_BLANC = 3;
-    const int FOU_BLANC = 4;
-    const int DAME_BLANC = 5;
-    const int ROI_BLANC = 6;
-
-    const int PION_NOIR = 7;
-    const int TOUR_NOIR = 8;
-    const int CAVALIER_NOIR = 9;
-    const int FOU_NOIR = 10;
-    const int DAME_NOIR = 11;
-    const int ROI_NOIR = 12;
     static std::vector<std::vector<uint64_t>> zobristTable;
     static uint64_t zobristBlackTurn;
 public:
